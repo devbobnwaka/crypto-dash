24741109b52068ea85fff1c565720811c359b211
Chibueze.Nwaka <Chibueze.Nwaka@Nlng.com>
2025-10-17 17:06:46 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+const API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error("Failed to fetch data");
+        const data = await res.json();
+        setCoins(data);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, []);
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((res) => {
+  //       if (!res.ok) throw new Error("Failed to fetch data");
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setCoins(data);
+  //       setLoading(false);
+  //     })
+  //     .catch((err) => {
+  //       setError(err.Message);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+    </div>
+  );
+};
+
+export default App;
