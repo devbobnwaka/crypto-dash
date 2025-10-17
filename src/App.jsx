b8680c484d34bcd6a004f7227aa86ecdc8d99fc8
Chibueze.Nwaka <Chibueze.Nwaka@Nlng.com>
2025-10-17 17:41:10 +0100

    Display coins

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
 const API_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
@@ -26,26 +27,19 @@ const App = () => {
     fetchCoins();
   }, []);
 
-  // useEffect(() => {
-  //   fetch(API_URL)
-  //     .then((res) => {
-  //       if (!res.ok) throw new Error("Failed to fetch data");
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setCoins(data);
-  //       setLoading(false);
-  //     })
-  //     .catch((err) => {
-  //       setError(err.Message);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">{error}</div>}
+
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <CoinCard key={coin.id} coin={coin} />
+          ))}
+        </main>
+      )}
     </div>
   );
 };
