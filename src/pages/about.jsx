4a3d9f1fe13df66e193f172265c2d1cb257795c9
Chibueze.Nwaka <Chibueze.Nwaka@Nlng.com>
2025-10-18 04:25:43 +0100

    not found page

---
+++ src/pages/about.jsx
@@ -0,0 +1,33 @@
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h1>About Crypto Dash</h1>
+      <p>
+        You can explore the top cryptocurrencies by market cap, filter by name
+        or symbol, and sort them by price, market cap or 24-hour change.
+      </p>
+      <p>
+        THis project is built as part of a React tutorial to help you understand
+        hooks, components, state management, and integrating with external APIs.
+      </p>
+      <p>
+        🚀 Future features might include detailed coin vies, favorites,
+        pagination, and much more
+      </p>
+      <p>
+        You can explore the top cryptocurrencies by market cap, filter by name
+        or symbol, and sort them by price, market cap or 24-hour change.
+      </p>
+      <p>
+        THis project is built as part of a React tutorial to help you understand
+        hooks, components, state management, and integrating with external APIs.
+      </p>
+      <p>
+        🚀 Future features might include detailed coin vies, favorites,
+        pagination, and much more
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
